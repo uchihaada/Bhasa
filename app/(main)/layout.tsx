6f413ec142345074c8fda06328021a61c5203c67
Abhijit Das <abhijitadadas@gmail.com>
2024-03-20 01:49:41 +0530

    Second

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+import { MobileHeader } from "@/components/MobileHeader";
+import { Sidebar } from "@/components/sidebar";
 
 
 type Props = {
@@ -9,10 +11,17 @@ const MainLayout = ({
 }: Props) => {
     return (
         <>
-            {children}
+            <MobileHeader />
+            <Sidebar className="hidden lg:flex" />
+            <main className="lg:pl-[256px] h-full pt-[50px] lg:pt-0">
+                <div className=" max-w-[1056px] mx-auto pt-6 h-full">
+
+                    {children}
+                </div>
+            </main>
         </>
 
     );
 }
 
-export default MainLayout;
+export default MainLayout; 
--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,9 +1,20 @@
-const LearnPage=()=>{
-return(
-<div>
-    Learn Page
-</div>
-);
+import { FeedWrapper } from "@/components/feedWrapper";
+import { StickyWrapper } from "@/components/stickyWrapper";
+
+const LearnPage = () => {
+    return (
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                My Sticky sidebar
+            </StickyWrapper>
+            <FeedWrapper>
+               My Feed
+
+               
+            </FeedWrapper>
+
+        </div >
+    );
 };
 
 export default LearnPage
