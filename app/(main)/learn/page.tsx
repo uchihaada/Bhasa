3ac276b214e1800000de68f008692d0497114f60
Abhijit Das <abhijitadadas@gmail.com>
2024-03-23 19:52:03 +0530

    New Components

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,16 +1,24 @@
 import { FeedWrapper } from "@/components/feedWrapper";
 import { StickyWrapper } from "@/components/stickyWrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/userProgress";
 
 const LearnPage = () => {
     return (
         <div className="flex flex-row-reverse gap-[48px] px-6">
             <StickyWrapper>
-                My Sticky sidebar
+                <UserProgress
+                    activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }}
+                    heart={5}
+                    points={100}
+                    hasActiveSubscription={false}
+                />
             </StickyWrapper>
             <FeedWrapper>
-               My Feed
+                <Header title="Spanish">
 
-               
+                </Header>
+
             </FeedWrapper>
 
         </div >
---
+++ db/schema.ts
@@ -0,0 +1,7 @@
+import { serial, text, boolean, pgTable } from "drizzle-orm/pg-core";
+
+export const courses = pgTable("courses", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull(),
+  imageSrc: text("image_src").notNull(),
+});
